6ba764195a66211f8cd175903322429be6307d72
Omar Luna <mrebel62@gmail.com>
2019-11-28 21:30:25 -0600

    most activities

--- back/app.js
+++ back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+let path = require('path');
 
 let app = express();
 const PORT =  process.env.PORT || 3000;
@@ -12,8 +13,28 @@ var reservations = [
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "interface.html"));
+});
+
+app.get('/reserve', (req, res) => {
+  res.sendFile(path.join(__dirname, 'interfaceReserve.html'));
+});
+
+app.get('/tables', (req, res) => {
+  res.sendFile(path.join(__dirname, 'interfaceTables.html'));
+  console.log(reservations);
+	// res.json(reservations);
+});
+
+app.get('/reserveData',(req,res) =>{
+  res.json(reservations);
+});
+
 app.post('/reserve', (req, res) => {
   var newReservation = req.body;
+  console.log(newReservation);
+  reservations.push(newReservation);
 
   res.json(newReservation);
 })
@@ -21,7 +42,3 @@ app.post('/reserve', (req, res) => {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-app.get('/tables', (_, res) => {
-	res.send(reservations);
-})
