5e0e6885d036d5105642c819bdc9e643ddc2e7a6
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2019-11-28 19:18:20 -0600

    port

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 let app = express();
+const PORT =  process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -8,3 +9,4 @@ app.use(express.json());
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
